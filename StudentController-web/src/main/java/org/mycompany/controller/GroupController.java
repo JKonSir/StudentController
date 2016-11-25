68bca5f406974ecb9b44748c5b5b824aeb3ba5f5
KonstantinAMP <gijoe403@gmail.com>
2016-11-26 00:34:43 +0400

    repositories were used instead TxUtils and EntityDao

--- StudentController-web/src/main/java/org/mycompany/controller/GroupController.java
+++ StudentController-web/src/main/java/org/mycompany/controller/GroupController.java
@@ -1,27 +1,94 @@
 package org.mycompany.controller;
 
-import org.mycompany.dao.EntityDao;
 import org.mycompany.model.Group;
-import org.mycompany.utils.StudentModelDependencyOnSpring;
-import org.mycompany.utils.TxUtils;
+import org.mycompany.model.Student;
+import org.mycompany.repositories.EntityRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
 
 @Controller
-@RequestMapping(value = "/group")
+@RequestMapping(value = RestUrlConst.BASE_URL)
 public class GroupController
 {
-    @CrossOrigin
-    @RequestMapping(value = "/group", method = RequestMethod.GET)
-    public @ResponseBody
-    Group getGroup(@RequestParam(name = "id") String id) throws Exception
-    {
-        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
-        final EntityDao<Group> groupEntityDao = StudentModelDependencyOnSpring.getInstance().getGroupDao();
+    @Inject
+    private EntityRepository<Group> groupRepository;
+    @Inject
+    private EntityRepository<Student> studentRepository;
 
-        return txUtils.doInTransactionRequired(() -> groupEntityDao.getEntity(new BigInteger(id)));
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.GROUP_URL,
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Group getGroupById(@PathVariable(value = "groupId") String groupId) throws Exception
+    {
+        return groupRepository.findById(new BigInteger(groupId), Boolean.FALSE);
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.ALL_GROUPS_URL,
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Group> getGroupsByIds(@RequestBody List<String> groupIds) throws Exception
+    {
+        return groupRepository.findByIds
+                (
+                        groupIds.stream()
+                                .map(BigInteger::new)
+                                .collect(Collectors.toList())
+                );
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.ALL_GROUPS_URL,
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Group> getAllGroups() throws Exception
+    {
+        return groupRepository.findAll();
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.ALL_STUDENTS_OF_GROUP_URL,
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Student> getStudentsOfGroup(@PathVariable(value = "groupId") String groupId) throws Exception
+    {
+        return groupRepository.findById(new BigInteger(groupId), Boolean.TRUE).getStudents();
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.STUDENT_OF_GROUP_URL,
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Student getStudentOfGroup(@PathVariable(value = "groupId") String groupId,
+                                           @PathVariable(value = "studentId") String studentId) throws Exception
+    {
+        List<Student> students = groupRepository.findById(new BigInteger(groupId), Boolean.TRUE).getStudents();
+        for (Student student : students)
+        {
+            if (new BigInteger(studentId).equals(student.getId()))
+            {
+                return student;
+            }
+        }
+        return null;
     }
 
 }
--- StudentController-web/src/main/java/org/mycompany/controller/StudentController.java
+++ StudentController-web/src/main/java/org/mycompany/controller/StudentController.java
@@ -1,74 +1,114 @@
 package org.mycompany.controller;
 
-import org.mycompany.dao.EntityDao;
-import org.mycompany.model.Group;
 import org.mycompany.model.Student;
-import org.mycompany.utils.StudentModelDependencyOnSpring;
-import org.mycompany.utils.TxUtils;
+import org.mycompany.repositories.EntityRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigInteger;
-import java.util.concurrent.Callable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
 
 @Controller
-@RequestMapping(value = "/student")
+@RequestMapping(value = RestUrlConst.BASE_URL)
 public class StudentController
 {
-    @CrossOrigin
-    @RequestMapping(value = "/get_student", method = RequestMethod.GET)
-    public @ResponseBody Student getStudent(@RequestParam(name = "id") String id) throws Exception
-    {
-        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
-        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
+    @Inject
+    private EntityRepository<Student> studentRepository;
 
-        return txUtils.doInTransactionRequired(() -> studentDao.getEntity(new BigInteger(id)));
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.STUDENT_URL,
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Student getStudentById(@PathVariable(value = "studentId") String studentId) throws Exception
+    {
+        return studentRepository.findById(new BigInteger(studentId), true);
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/delete_student", method = RequestMethod.DELETE)
-    public @ResponseBody void deleteStudent(@RequestParam(name = "id") BigInteger id) throws Exception
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.ALL_STUDENTS_URL,
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Student> getStudentsByIds(@RequestBody List<String> studentIds)
     {
-        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
-        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
-
-        txUtils.doInTransactionRequired((Callable<Student>) () -> {
-            Student student = studentDao.getEntity(id);
-            studentDao.delete(student);
-            return null;
-        });
+        return studentRepository.findByIds(
+                studentIds.stream()
+                        .map(BigInteger::new)
+                        .collect(Collectors.toList())
+        );
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/update_student", method = RequestMethod.PUT)
-    public @ResponseBody void updateStudent(Student student) throws Exception
+    @ResponseBody
+    @RequestMapping(value = RestUrlConst.ALL_STUDENTS_URL,
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Student> getAllStudents()
     {
-        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
-        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
-
-        txUtils.doInTransactionRequired((Callable<Student>) () -> {
-            studentDao.createOrUpdate(student);
-            return null;
-        });
+        return studentRepository.findAll();
     }
 
-    @CrossOrigin
-    @RequestMapping(value = "/add_student/{groupId}", method = RequestMethod.POST)
-    public @ResponseBody void addStudent(@PathVariable BigInteger groupId, @RequestBody Student addStudent) throws Exception
-    {
-        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
-        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
-        final EntityDao<Group> groupDao = StudentModelDependencyOnSpring.getInstance().getGroupDao();
 
-        System.out.println(addStudent);
-
-        txUtils.doInTransactionRequired((Callable<Student>) () -> {
-            Group group = groupDao.getEntity(groupId);
-            Student student = new Student(addStudent.getFirstName(), addStudent.getLastName(),
-                    addStudent.getAge(), group);
-            studentDao.createOrUpdate(student);
-            return null;
-        });
-    }
+//    @CrossOrigin
+//    @RequestMapping(value = RestUrlConst.STUDENT_URL + "/{studentId}", method = RequestMethod.DELETE)
+//    public @ResponseBody void deleteStudent(@RequestParam(name = "studentId") String studentId) throws Exception
+//    {
+//        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
+//        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
+//
+//        txUtils.doInTransactionRequired((Callable<Student>) () -> {
+//            Student student = studentDao.getEntity(new BigInteger(studentId));
+//            studentDao.delete(student);
+//            return null;
+//        });
+//    }
+//
+//    @CrossOrigin
+//    @RequestMapping(value = RestUrlConst.STUDENT_URL + "/{studentId}", method = RequestMethod.POST)
+//    public @ResponseBody void updateStudent(@PathVariable(value = "studentId") String studentId, Student student) throws Exception
+//    {
+//        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
+//        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
+//
+//        if (studentId != null)
+//        {
+//            student.setId(new BigInteger(studentId));
+//        }
+//
+//        txUtils.doInTransactionRequired((Callable<Student>) () -> {
+//            studentDao.createOrUpdate(student);
+//            return null;
+//        });
+//    }
+//
+//    @CrossOrigin
+//    @RequestMapping(value = RestUrlConst.STUDENT_URL, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public @ResponseBody void createStudent(@PathVariable(value = "groupId") String groupId,
+//                                            @RequestBody Student student) throws Exception
+//    {
+//        final TxUtils txUtils = StudentModelDependencyOnSpring.getInstance().getTxUtils();
+//        final EntityDao<Student> studentDao = StudentModelDependencyOnSpring.getInstance().getStudentDao();
+//        final EntityDao<Group> groupDao = StudentModelDependencyOnSpring.getInstance().getGroupDao();
+//
+//        System.out.println(student);
+//
+//        txUtils.doInTransactionRequired((Callable<Student>) () -> {
+//            Group group = groupDao.getEntity(new BigInteger(groupId));
+//            Student result = new Student(student.getFirstName(), student.getLastName(),
+//                    student.getAge(), group);
+//            studentDao.createOrUpdate(result);
+//            return null;
+//        });
+//    }
 
 }
